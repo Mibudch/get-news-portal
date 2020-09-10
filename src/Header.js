0fbebea36ec5d26b3dd0dfbc8030376c5b42bd35
Shag <>
2020-09-10 21:15:02 +0300

    Trying to get geolocation

--- src/Header.js
+++ src/Header.js
@@ -10,33 +10,57 @@ class Header extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            temperature: [],
+            test: 'test',
+            isLoading: false,
+            lat: 53.893009,
+            lon: 27.567444,
+            temperMin: '',
+            temperMax: '',
             icon: ''
         }
     }
+
     componentDidMount() {
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Mogilev&units=metric&lang=ru&appid=8deb1290960a6df846daf0a26e878871`)
+        navigator.geolocation.getCurrentPosition(position => {
+            const lat = position.coords.latitude
+            const lon = position.coords.longitude
+            this.setState({ lat: lat, lon: lon })
+        })
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&units=metric&lang=ru&appid=8deb1290960a6df846daf0a26e878871`)
             .then(res => {
-                const temperature = res.data
+                const temperMin = res.data.main.temp_min
+                const temperMax = res.data.main.temp_max
                 const icon = res.data.weather[0].icon
-                this.setState({ temperature, icon })
+                this.setState({ temperMin, icon, temperMax, isLoading: true })
                 console.log(res.data);
             })
+
     }
+    // componentDidUpdate(prevProps, prevState) {
+
+    //         if (this.state.lon !== prevState.lon && this.state.lat !== prevState.lat) {
+    //             this.setState({ lat: lat, lon: lon })
+
+    //         }
+        
+    // }
     render() {
-        return (
-            <header>
-                <div className='header header_flex'>
-                    <Logo />
-                    <Finder />
-                    <WeatherСurrency
-                        icon={this.state.icon}
-                    />
-                </div>
-                <Ticker />
-                <TagWrapper />
-            </header>
-        )
-    }
+            // console.log(this.state.lat, this.state.lon)
+            return !this.state.isLoading ? null : (
+                <header>
+                    <div className='header header_flex'>
+                        <Logo />
+                        <Finder />
+                        <WeatherСurrency
+                            icon={this.state.icon}
+                            temperMin={this.state.temperMin}
+                            temperMax={this.state.temperMax}
+                        />
+                    </div>
+                    <Ticker />
+                    <TagWrapper />
+                </header>
+            )
+        }
 }
-export default Header
+    export default Header
--- src/Header/WeatherСurrency.js
+++ src/Header/WeatherСurrency.js
@@ -2,9 +2,10 @@ import React from 'react'
 function WeatherСurrency(props) {
     return(
         <h3>
-            <div>Погода: <img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt=''></img></div>
+            <div>Погода:<img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt=''></img> {props.temperMin}C&deg; - {props.temperMax}C&deg;</div>
             <div>Курсы НБРБ:</div>
         </h3>
     )
+    
 }
 export default WeatherСurrency
