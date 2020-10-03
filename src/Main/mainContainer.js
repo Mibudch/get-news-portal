70fa0353065b17fb6c24b0d12d35edcc32de5f35
Mikhail Budzilovich <58990078+Mibudch@users.noreply.github.com>
2020-10-03 15:04:09 +0300

    1

--- src/Main/mainContainer.js
+++ src/Main/mainContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { tagArray } from '../sys/TagArray.js'
+import MainSection from './mainSection.js'
 import { getTopNewsAPI, getTopBusinessNewsAPI, getTopTechnologyNewsAPI, getTopEntertainmentNewsAPI, getTopScienceNewsAPI, getTopHealthNewsAPI, getTopSportsNewsAPI } from '../sys/sysAPI'
 class MainContainer extends Component {
     constructor(props) {
@@ -13,7 +14,8 @@ class MainContainer extends Component {
             topScienceNews: [],
             topHealthNews: [],
             topSportsNews: [],
-            categoryContent: []
+            allNews: [],
+            newsCategoryArray: []
         }
     }
     async componentDidMount() {
@@ -27,20 +29,41 @@ class MainContainer extends Component {
                 getTopHealthNewsAPI(),
                 getTopSportsNewsAPI(),
             ])
-            const { categoryContent } = this.state
-            categoryContent.push(getTopNews.data.articles, getTopBusinessNews.data.articles, getTopTechnologyNews.data.articles, getTopEntertainmentNews.data.articles, getTopScienceNews.data.articles, getTopHealthNews.data.articles, getTopSportsNews.data.articles)
-            categoryContent.map((elem, ind) => {
-                return elem.map(el => el.category = tagArray[ind].category)
-            })
-            this.setState({ categoryContent })
+            const { allNews } = this.state
+            allNews.push(getTopNews.data.articles, getTopBusinessNews.data.articles, getTopTechnologyNews.data.articles, getTopEntertainmentNews.data.articles, getTopScienceNews.data.articles, getTopHealthNews.data.articles, getTopSportsNews.data.articles)
+            allNews.map((elem, ind) => elem.map(el => el.category = tagArray[ind].category))
+            const getNewsCategoryArray = () => (
+                allNews.map((el, i) => el[i].category)
+            )
+            console.log(getNewsCategoryArray());
+            this.setState({ allNews, newsCategory: getNewsCategoryArray(), isLoading: true })
         } catch (e) {
             console.log(e);
         }
     }
+    getNewsCategoryArray = () => {
+        const { allNews } = this.state
+        return allNews.map((el, i) => {
+            return el[i].category
+        })
+    }
+    getMainNews = () => {
+        const { allNews } = this.state
+        return allNews.map(el => el[0])
+    }
+    getTopSideNewsArray = () => {
+        const { allNews } = this.state
+        return allNews.map(el => el.slice(1, 5))
+    }
     render() {
-        console.log(this.state.categoryContent);
-        return (
-            <div>MainContainer</div>
+        // console.log(this.state.newsCategoryArray);
+        return this.state.isLoading && (
+            <MainSection
+                newsContent={this.state.allNews.map(el => el)}
+                newsCategory={this.getNewsCategoryArray().map(el => el)}
+                mainNews={this.getMainNews()}
+                topSideNews={this.getTopSideNewsArray()}
+            />
         )
     }
 }
